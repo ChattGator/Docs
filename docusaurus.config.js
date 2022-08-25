e4aea1ef1cea65a93ba20de284ce6f232fb48c15
herumbsimplified <herumb@simplified.co>
2022-08-25 23:58:08 +0530

    Chalga ab toh

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,11 +6,10 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "ChattGator",
+  tagline: "Chat Integration for Everyone",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
-  onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
